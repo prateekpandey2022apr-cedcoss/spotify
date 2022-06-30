76aefa9ae80aff023fe238b83294a5e59972e285
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-06-30 15:26:15 +0530

    component 4

--- src/Card.js
+++ src/Card.js
@@ -2,20 +2,38 @@ import React from "react";
 
 class Card extends React.Component {
   render() {
+    console.log(this.props);
     return (
       <div className="col-md-6 col-lg-3">
         <div className="xcard">
-          <p>
-            <img
-              src={this.props.path}
-              width={142}
-              className="card-"
-              alt="this is alt"
-            />
-          </p>
           <div className="">
+            {this.props.trial && (
+              <p>
+                <span className="bg-primary text-white p-2 border border-primary rounded">
+                  1 month free
+                </span>
+              </p>
+            )}
+            <p className="border border-primary p-1 rounded">
+              One-time plans available
+            </p>
             <h5 className="">{this.props.title}</h5>
-            <small>{this.props.desc}</small>
+            <small>{this.props.info_1}</small> <br />
+            <small>{this.props.info_2}</small>
+          </div>
+
+          <hr />
+
+          {this.props.children}
+
+          <div className="d-grid">
+            <button type="button" className="btn btn-dark rounded-pill">
+              VIEW PLANS
+            </button>
+          </div>
+
+          <div className="terms mt-3">
+            <small>{this.props.terms}</small>
           </div>
         </div>
       </div>
--- src/Main.js
+++ src/Main.js
@@ -4,29 +4,98 @@ import Card from "./Card";
 class Main extends React.Component {
   render() {
     return (
-      <div className="row text-center main">
-        <h2 className="mb-5">Connect with music culture</h2>
-        <Card
-          path="./component-3/images/img_1.png"
-          title="Bollywood Butter."
-          desc="The Finest quality bollywood music picked up for you."
-        />
-        <Card
-          path="./component-3/images/img_2.png"
-          title="Pubjabi 101."
-          desc="The ultimate pubjabi hits."
-        />
-        <Card
-          path="./component-3/images/img_3.png"
-          title="Tollywood Pearls."
-          desc="The finest music from Telugu films."
-        />
-        <Card
-          path="./component-3/images/img_4.png"
-          title="Hot Hits India."
-          desc="50 hottest hits from across the globe."
-        />
-      </div>
+      <>
+        <div className="row main">
+          <div className="intro text-center mb-4">
+            <h2 className="">
+              <i className="bi bi-check"></i>Pick your Premium
+            </h2>
+            <p>
+              Listen without limits on your phone, speaker, and other devices
+            </p>
+            <p className="payment-options">
+              <img src="./component-4/images/po-1.svg" alt="upi" />
+              <img src="./component-4/images/po-2.svg" alt="paytm" />
+              <img src="./component-4/images/po-3.svg" alt="visa" />
+              <img src="./component-4/images/po-4.svg" alt="mc" />
+              <img src="./component-4/images/po-5.svg" alt="amex" />
+            </p>
+          </div>
+          <Card
+            trial={false}
+            offer="1 month free"
+            title="Mini."
+            info_1="From &#8377;7/day"
+            info_2="1 account"
+            terms="Terms and conditions apply"
+          >
+            <ul>
+              <li>Add free music listening on mobile.</li>
+              <li>Group Session.</li>
+              <li>Download 30 songs on 1 mobile device.</li>
+              <li>Download 10k songs/device on 5 devices per account.</li>
+            </ul>
+          </Card>
+          <Card
+            path="./component-3/images/img_1.png"
+            trial={true}
+            offer="1 month free"
+            title="Duo"
+            info_1="From &#8377;149/month after trial period"
+            info_2="2 account"
+            terms="Terms and conditions apply. 1 month free not available for users who have already tried Premium."
+          >
+            <ul>
+              <li>Add free music listening on mobile.</li>
+              <li>Group Session.</li>
+              <li>Download 10k songs/device on 5 devices.</li>
+              <li>Download 10k songs/device on 5 devices per account.</li>
+            </ul>
+          </Card>
+          <Card
+            path="./component-3/images/img_1.png"
+            trial={true}
+            offer="1 month free"
+            title="Individual"
+            info_1="From &#8377;119/month after trial period"
+            info_2="2 account"
+            terms="Terms and conditions apply. 1 month free not available for users who have already tried Premium."
+          >
+            <ul>
+              <li>For couples who live together.</li>
+              <li>Add free music listening on mobile.</li>
+              <li>Group Session.</li>
+              <li>Download 10k songs/device on 5 devices per account.</li>
+            </ul>
+          </Card>
+          <Card
+            path="./component-3/images/img_1.png"
+            trial={true}
+            offer="1 month free"
+            title="Family"
+            info_1="From &#8377;179/month after trial period"
+            info_2="Up to 6 accounts"
+            terms="Terms and conditions apply. 1 month free not available for users who have already tried Premium."
+          >
+            <ul>
+              <li>For couples who live together.</li>
+              <li>Add free music listening on mobile.</li>
+              <li>Group Session.</li>
+              <li>Download 10k songs/device on 5 devices per account.</li>
+            </ul>
+          </Card>
+        </div>
+        <div className="row mb-3">
+          <div className="offer text-center mx-auto">
+            <p>
+              Special discount for eligible students in university &nbsp;
+              <a href="/" className="btn btn-outline-dark rounded-pill">
+                LEARN MORE
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
     );
   }
 }
